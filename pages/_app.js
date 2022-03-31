a5735ae8344f6be056f0fccb220989f95eb4a67b
Nick H <nickdog1204@gmail.com>
2022-03-31 19:52:38 +0800

    clear the demo: "project starter"

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return (
+        <div>
+            <Component {...pageProps} />
+            {/*<footer>*/}
+            {/*    <p>© 2018 Gandalf</p>*/}
+            {/*</footer>*/}
+        </div>
+    )
 }
 
 export default MyApp
